3929d1a8dcb92ca787d3939047684b6a6b1d7986
Obumuneme Nwabude <obumnwabude@gmail.com>
2020-10-01 18:23:25 +0100

    implement airtime atlas API

--- public/assets/js/airtime.js
+++ public/assets/js/airtime.js
@@ -8,6 +8,22 @@ airtimeForm.addEventListener('submit', async (e) => {
   e.preventDefault();
   const formBody = Object.fromEntries(new FormData(airtimeForm));
   if (formBody.amount >= 100 && formBody.amount <= 10000 && /^\+234[789][01]\d{8}$/.test(formBody.phone)) {
+    // call airtime API
+    await fetch('https://sandboxapi.fsi.ng/atlabs/airtime/send', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Sandbox-Key': `${await (await fetch('/sandbox')).text()}`
+      },
+      body: JSON.stringify({
+        'recipients': [{
+          'phoneNumber': formBody.phone,
+          'amount': formBody.amount,
+          'currencyCode': 'NGN'
+        }]
+      })
+    }).then((response) => response.json())
+    .catch((error) => console.log(error));
     alert(`Dear ${firebase.auth().currentUser.displayName},\nYou have successfully purchased NGN${formBody.amount} for ${formBody.phone}`);
   } else {
     alert('Something went wrong, please try again!');
@@ -15,3 +31,4 @@ airtimeForm.addEventListener('submit', async (e) => {
   phone.value = '';
   amount.value = '';
 });
+
